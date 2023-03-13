60f739790e6e2b9449a3ff391f9e0767c49e9dfd
Ricardo <ricardo.spinola9@gmail.com>
2023-03-12 22:56:00 -0300

    sign in google

--- src/Apps/Capitulo1/components/organisms/Home/components/Main/index.jsx
+++ src/Apps/Capitulo1/components/organisms/Home/components/Main/index.jsx
@@ -5,14 +5,17 @@ import {
     createDroppable,
   } from "@thisbeyond/solid-dnd";
 
+import { createMemo, createEffect } from 'solid-js';
 import { useStore } from '../../storage';
 import { CLASS_NAME } from '../../storage';
 import { createModal } from '@/Apps/Capitulo1/components/molecules/Modal';
-import { createEffect } from 'solid-js';
 
 import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
 import { relaunch } from '@tauri-apps/api/process'
-  
+ 
+import { useLocation, useNavigate } from '@solidjs/router';
+
+
 const Draggable = ({id, drop}) => {
     const draggable = createDraggable(id,{drop:drop});
     return (
@@ -76,13 +79,61 @@ const ModalUpdate=()=>{
     )
 }
 
+const ModalSignin=()=>{
+
+
+
+ 
+    function sign(){
+        
+    }
+
+    return (
+        <div style={{display:'flex', width:'100px', height:'100px', all:'unset'}}>
+            <div id="g_id_onload"
+                data-client_id="1056413810692-eoq4oklo337jq5vrcbk8h42ng52odhml.apps.googleusercontent.com"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="getLogin"
+                data-itp_support="true"
+                >
+            </div>
+
+            <div class="g_id_signin"
+                data-type="standard"
+                data-shape="rectangular"
+                data-theme="filled_black"
+                data-text="signin_with"
+                data-size="medium"
+                data-locale="pt-BR"
+                data-logo_alignment="left">
+            </div>
+        </div>
+    )
+}
+
 export default function Main(){
 
     const inside = useStore(state=>state.dados.inside)
 
+    const navigate = useNavigate()
+
+    const location = useLocation();
+    const pathname = createMemo(() => location.pathname);
+
+    createEffect(()=>{
+        console.log(pathname())
+    })
+
     const { open, close } = createModal(<div className="flex w-52 h-32 background-white-fundo rounded-2xl items-center justify-center">
         <ModalUpdate/>
     </div>)
+
+    const { open:openSign, close:CloseSign } = createModal(<div className="flex w-52 h-32 background-white-fundo rounded-2xl items-center justify-center">
+        <ModalSignin/>
+    </div>)
+
+openSign()
 
     return (
         <div className={style.main} id="main_content">
@@ -104,8 +155,13 @@ export default function Main(){
                     <svg className='icon-svg' width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 22q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.263 6.913T12 22Z"/></svg>
                     <p>Modal 1.3.0</p>
                 </button>
-            </div>
 
+                <button onClick={openSign}>
+                    <svg className='icon-svg' width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 22q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.263 6.913T12 22Z"/></svg>
+                    <p>Sign in</p>
+                </button>
+            </div>
+            <button onClick={()=>navigate("/login")}>change route</button>
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App";
+import { Router } from "@solidjs/router";
 
-render(() => <App />, document.getElementById("root"));
+render(() => 
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById("root")
+);
