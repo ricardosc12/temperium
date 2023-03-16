7575d80ce4c010cbe741fdb41f0ca590102ecd87
Ricardo <ricardo.spinola9@gmail.com>
2023-03-16 20:24:36 -0300

    ajuste texto

--- src/Apps/Capitulo1/components/organisms/Login/Modal/index.jsx
+++ src/Apps/Capitulo1/components/organisms/Login/Modal/index.jsx
@@ -49,7 +49,7 @@ export default function ModalSign(){
                     </h4>
                 </div>
                 <div className='mt-5'>
-                    <h3>1. Objetivo</h3>
+                    <h3>2. Objetivo</h3>
                     <h4 className='color-text-secondary p-1 indent-4 text-justify'>
                         Com o objetivo de construir uma ferramenta que atenda às necessidades dos estudantes, optamos por utilizar uma abordagem de design participativo. 
                         Dessa forma, entregaremos uma ferramenta que esteja o mais próximo possível das demandas existentes.
@@ -65,7 +65,7 @@ export default function ModalSign(){
 
                     <div className='w-full flex flex-col justify-center items-center mt-5'>
                         <h3>Entre com sua conta universitária para participar.</h3>
-                        <button className='btn-base bg-black-dark pl-0 mt-5' onClick={sign}>
+                        <button className='btn-base bg-black-dark pl-0 py-0 mt-5 h-7' onClick={sign}>
                             <div className='bg-text-primary rounded-sm p-1 rounded-r-none mr-2'>
                                 <svg width="17" height="17" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
                             </div>
