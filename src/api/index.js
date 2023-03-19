027701094cbe09ca8da4c17392e396c2e197c17f
Ricardo <ricardo.spinola9@gmail.com>
2023-03-19 15:42:37 -0300

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,11 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 
 class ClientDB {
     static client () {
-        return new DynamoDBClient({ region: "us-east-2",
-        credentials: {
-            accessKeyId: "AKIAYGR6C56R635ZSB66",
-            secretAccessKey: "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"
-        }});
+        return new DynamoDBClient({ region: "us-east-2"})
+        // credentials: {
+            // accessKeyId: "AKIAYGR6C56R635ZSB66",
+            // secretAccessKey: "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"
+        // }});
     }
 }
 
