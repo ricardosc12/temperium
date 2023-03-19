adb5b3c218d7bc3f987a5fdd054ffd0ede81e586
Ricardo <ricardo.spinola9@gmail.com>
2023-03-19 15:46:09 -0300

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ class ClientDB {
     static client () {
         return new DynamoDBClient({ region: "us-east-2"})
         // credentials: {
-            // accessKeyId: "AKIAYGR6C56R635ZSB66",
-            // secretAccessKey: "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"
+            // accessKeyId: "******",
+            // secretAccessKey: "*****"
         // }});
     }
 }
