fe42e6c7e93578d8cb0fbcca87ead1093ac5ddaa
Ricardo <ricardo.spinola9@gmail.com>
2023-03-19 15:56:12 -0300

    access key env

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,12 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 
 class ClientDB {
     static client () {
-        return new DynamoDBClient({ region: "us-east-2"})
-        // credentials: {
-            // accessKeyId: "******",
-            // secretAccessKey: "*****"
-        // }});
+        console.log(import.meta.env)
+        return new DynamoDBClient({ region: "us-east-2",
+        credentials: {
+            accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+            secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+        }});
     }
 }
 
