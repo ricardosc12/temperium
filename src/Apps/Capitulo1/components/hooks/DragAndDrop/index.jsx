8f0ef1f10327704f17c7207f53fc3b4d5ea8e08b
Ricardo <ricardo.spinola9@gmail.com>
2023-05-09 20:33:46 -0300

    feat(dnd) !! events enter & leave

--- src/Apps/Capitulo1/components/hooks/DragAndDrop/index.jsx
+++ src/Apps/Capitulo1/components/hooks/DragAndDrop/index.jsx
@@ -25,6 +25,7 @@ const DragAndDrop = (props) => {
 
     const handleDrop = (e) => {
         counter = 0;
+        auxCount = 0;
         if (!actual_element) return
         e.preventDefault()
         const dropper = e.target.closest("[name='droppable']")
@@ -42,57 +43,107 @@ const DragAndDrop = (props) => {
         )
     }
 
+    function handleLeave(e) {
+        console.time('ondragleave')
+        const droppable = e.target.closest('[name="droppable"]')
+        if (droppable) {
+            auxCount--;
+            if (auxCount == 0) {
+                props.onDragLeave && props.onDragLeave({
+                    draggable: {
+                        el: actual_element,
+                        data: actual_element.getAttribute('data-drag')
+                    },
+                    droppable: {
+                        el: droppable,
+                        data: droppable.getAttribute('data-drag')
+                    },
+                })
+            }
+        }
+        console.timeEnd('ondragleave')
+    }
+
+    function handleEnter(e) {
+        console.time('ondragenter')
+        const droppable = e.target.closest('[name="droppable"]')
+        if (droppable) {
+            auxCount++;
+            if (auxCount == 1) {
+                props.onDragEnter && props.onDragEnter(
+                    {
+                        draggable: {
+                            el: actual_element,
+                            data: actual_element.getAttribute('data-drag')
+                        },
+                        droppable: {
+                            el: droppable,
+                            data: droppable.getAttribute('data-drag')
+                        },
+                    }
+                )
+            }
+        }
+        console.timeEnd('ondragenter')
+    }
+
 
     onCleanup(() => {
         document.removeEventListener("dragstart", handleStart, false)
         document.removeEventListener('dragend', handleDragEnd, false)
         document.removeEventListener('dragover', handleDragOver, false)
+        document.removeEventListener("dragenter", handleEnter, false)
+        document.removeEventListener("dragleave", handleLeave, false)
     })
+
+    let auxCount = 0
 
     createEffect(() => {
         setTimeout(() => {
             console.time('onmount')
-            const droppable = document.getElementsByName('droppable')
+            // const droppable = document.getElementsByName('droppable')
             document.addEventListener("dragstart", handleStart, false)
             document.addEventListener("drop", handleDrop, false)
-            for (const dropper of droppable) {
-                dropper.ondragleave = (e) => {
-                    counter--;
-                    if (counter == 0) {
-                        console.time('ondragleave')
-                        props.onDragLeave&&props.onDragLeave({
-                            draggable: {
-                                el: actual_element,
-                                data: actual_element.getAttribute('data-drag')
-                            },
-                            droppable: {
-                                el: dropper,
-                                data: dropper.getAttribute('data-drag')
-                            },
-                        })
-                        console.timeEnd('ondragleave')
-                    }
+            document.addEventListener("dragenter", handleEnter, false)
+            document.addEventListener("dragleave", handleLeave, false)
+            // for (const dropper of droppable) {
+            //     dropper.ondragleave = (e) => {
+            //         counter--;
+            //         if (counter == 0) {
+            //             console.time('ondragleave')
+            //             props.onDragLeave&&props.onDragLeave({
+            //                 draggable: {
+            //                     el: actual_element,
+            //                     data: actual_element.getAttribute('data-drag')
+            //                 },
+            //                 droppable: {
+            //                     el: dropper,
+            //                     data: dropper.getAttribute('data-drag')
+            //                 },
+            //             })
+            //             console.timeEnd('ondragleave')
+            //         }
 
-                }
-                dropper.ondragenter = (e) => {
-                    counter++;
-                    if (counter != 1) return
-                    console.time('ondragenter')
-                    props.onDragEnter&&props.onDragEnter(
-                        {
-                            draggable: {
-                                el: actual_element,
-                                data: actual_element.getAttribute('data-drag')
-                            },
-                            droppable: {
-                                el: dropper,
-                                data: dropper.getAttribute('data-drag')
-                            },
-                        }
-                    )
-                    console.timeEnd('ondragenter')
-                }
-            }
+            //     }
+            //     dropper.ondragenter = (e) => {
+            //         counter++;
+            //         if (counter != 1) return
+            //         console.time('ondragenter')
+            //         props.onDragEnter&&props.onDragEnter(
+            //             {
+            //                 draggable: {
+            //                     el: actual_element,
+            //                     data: actual_element.getAttribute('data-drag')
+            //                 },
+            //                 droppable: {
+            //                     el: dropper,
+            //                     data: dropper.getAttribute('data-drag')
+            //                 },
+            //             }
+            //         )
+            //         console.timeEnd('ondragenter')
+            //     }
+            // }
             document.addEventListener('dragend', handleDragEnd, false)
             document.addEventListener('dragover', handleDragOver, false)
             console.timeEnd('onmount')
