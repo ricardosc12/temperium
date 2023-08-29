835a191e1f17ec35e3908c9f3c70c2e1643e4dcf
Ricardo <ricardo.spinola9@gmail.com>
2023-08-29 20:57:30 -0300

    window shadow & tauri permissions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,10 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::PathBuf;
+use tauri::Manager;
+use window_shadows::set_shadow;
+
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -38,6 +42,11 @@ fn save(name: &str) -> bool {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet, save])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
